2111083a89f36417c81e79b73e639ecc2f086f21
Soliman162 <ahmedsliman215@gmail.com>
2023-10-22 23:42:16 +0300

    edit driver

--- recipes-kernel/seg7_driver/files/seg7_display_mod.c
+++ recipes-kernel/seg7_driver/files/seg7_display_mod.c
@@ -7,6 +7,7 @@
 #include <linux/uaccess.h>
 #include <linux/gpio.h>
 #include <linux/of.h>
+#include <linux/slab.h>
 #include <linux/platform_device.h>
 
 #include "seg7_display_mod.h"
@@ -21,8 +22,7 @@ static struct cdev seg7_struct ;
 static struct class *device_class;
 static struct device *device_struct;
 static dev_t first_assigned_number = 0;
-static struct gpio *(*seg7_pins)[GPIO_PINS_NUMBER] = NULL;
-
+static struct gpio (*seg7_pins)[GPIO_PINS_NUMBER] = NULL;
 const unsigned char SSD_u8Numbers[10] = {0b00111111,0b00000110,0b01011011,0b01001111,0b01100110,0b01101101,0b01111101,0b00000111,0b01111111,0b01101111};
 
 static struct of_device_id of_7_segment_match[] = 
@@ -89,15 +89,15 @@ ssize_t write_module(struct file *file_mod, const char __user *user_buffer, size
 	seg7_index = write_buffer[DEVICE_INDEX]-'0';
 
 	if( 
-		seg7_pins[seg7_index] != NULL	  &&
-		((write_buffer[NUMBER_INDEX]>='0') && (write_buffer[NUMBER_INDEX]<='9')) &&
-		( (seg7_index >=0) && (seg7_index<device_counter) )
+		!IS_ERR_OR_NULL(seg7_pins[seg7_index]) &&
+		( (seg7_index >=0) && (seg7_index<device_counter) )		&&
+		((write_buffer[NUMBER_INDEX]>='0') && (write_buffer[NUMBER_INDEX]<='9'))
 	  )
 	{
 		seg7_num = SSD_u8Numbers[write_buffer[NUMBER_INDEX]-'0'];
 		for( seg7_pin=0;seg7_pin<GPIO_PINS_NUMBER;seg7_pin++ )
 		{
-			gpio_set_value((*seg7_pins)[seg7_index][seg7_pin].gpio,GET_BIT(seg7_num,seg7_pin)); 
+			gpio_set_value(seg7_pins[seg7_index][seg7_pin].gpio,GET_BIT(seg7_num,seg7_pin)); 
 		}
 	}
 	else
@@ -125,18 +125,18 @@ int module_probe(struct platform_device *pdev)
 	struct device_node *Child_node_Iter = NULL;
 	const struct device_node *parent_node = seg7->of_node;
 
-			/*check for another child node*/
 	while( (Child_node_Iter = of_get_next_child(parent_node,Child_node_Iter)) != NULL )
 	{
 		pr_info("got child node %s\n",Child_node_Iter->name);
 		/*reallocate size for new node pins*/
-		seg7_pins = (struct gpio * (*)[GPIO_PINS_NUMBER])krealloc(seg7_pins,sizeof(struct gpio * (*)[GPIO_PINS_NUMBER] )*(device_counter+1),GFP_KERNEL);
-		if( seg7_pins == NULL )
+		seg7_pins = (struct gpio (*) [GPIO_PINS_NUMBER])krealloc(seg7_pins,sizeof(struct gpio (*) [GPIO_PINS_NUMBER])*(device_counter+1),GFP_KERNEL);
+		if( IS_ERR_OR_NULL(seg7_pins) ) 
 		{
 			pr_err("Couldn't allocate memory for node %d\n",device_counter);
 			ret = -ENOMEM;
 			goto GET_PROPERETY_ERROR;
 		}
+		pr_info("memory allocated for node%d with size of %ld\n",device_counter,sizeof(struct gpio));
 		/*read pins from property*/
 		ret = of_property_read_u8_array(Child_node_Iter,"pins",Pins_array,GPIO_PINS_NUMBER);
 		if( ret != 0 )
@@ -149,11 +149,12 @@ int module_probe(struct platform_device *pdev)
 		{
 			pr_info("start set pin %d\n",i);
 			Pin_label = kasprintf(GFP_KERNEL,"pin%c",('A'+i));
-			(*seg7_pins)[device_counter][i] = (struct gpio){Pins_array[i],GPIOF_OUT_INIT_LOW,Pin_label};
+			seg7_pins[device_counter][i] = (struct gpio){Pins_array[i],GPIOF_OUT_INIT_LOW,Pin_label};
 			pr_info("finish set pin %d with label %s\n",i,Pin_label);
+			// kfree(Pin_label);
 		}
 		/*init pins*/
-		ret = gpio_request_array((*seg7_pins)[device_counter],GPIO_PINS_NUMBER);
+		ret = gpio_request_array(seg7_pins[device_counter],GPIO_PINS_NUMBER);
 		if( ret != 0 )
 		{
 			pr_err("gpio request failed\n");
@@ -167,9 +168,10 @@ int module_probe(struct platform_device *pdev)
 		pr_err("parent node is empty \n");
 		return -EINVAL;
 	}
+
 	pr_info("Finish setting up the node pins\n");
 	GET_CHILD_ERROR:
-		kfree((*seg7_pins)[device_counter]);
+		kfree(seg7_pins);
 	GET_PROPERETY_ERROR:
 		of_node_put(Child_node_Iter);
 		return ret;
@@ -180,8 +182,9 @@ void module_shutdown(struct platform_device *pdev)
 	int i;
 	for(i=0;i<device_counter;i++)
 	{
-		gpio_free_array((*seg7_pins)[i],GPIO_PINS_NUMBER);
+		gpio_free_array(seg7_pins[i],GPIO_PINS_NUMBER);
 	}
+
 	kfree(seg7_pins);
 }
 
@@ -203,7 +206,7 @@ static int __init seg_7display_init(void)
 		goto DELETE_DEVICE_NUMBER;
 	}
 	device_class = class_create(THIS_MODULE,CLASS_NAME);
-	if( IS_ERR(device_class))
+	if(IS_ERR(device_class))
 	{
 		pr_err("device class failed\n");
 		ret_value = PTR_ERR(device_class);
